36dfd53aebf52840ac25d14c3e0e0ca3b38e7ba0
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-02-27 00:43:19 +0600

    add phone authentication

---
+++ libs/firebase/index.ts
@@ -0,0 +1,16 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCgV_Mwr7X9plYqhERsGEqxmDNp1vqLwLg',
+  authDomain: 'jmart-app-44a21.firebaseapp.com',
+  projectId: 'jmart-app-44a21',
+  storageBucket: 'jmart-app-44a21.appspot.com',
+  messagingSenderId: '455227918808',
+  appId: '1:455227918808:web:671333975d1b106c4a8aad',
+  measurementId: 'G-TS4QTTPK9C',
+};
+
+// Initialize Firebase
+if (!firebase) {
+}
+const app = initializeApp(firebaseConfig);
